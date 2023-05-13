c572cbdcb5b90e0db25ce5e887188f9af28f6228
NotAsExpected <62259187+BombaskiGeneralZPodlasia@users.noreply.github.com>
2023-05-13 19:36:14 +0100

    GET_MEM_SIZE_PHYS

--- CPP/RAM_size/main.cpp
+++ CPP/RAM_size/main.cpp
@@ -1,15 +1,12 @@
-#include <iostream>
 #include <windows.h>
 
-int main()
+int GET_MEM_SIZE_PHYS()
 { 
  MEMORYSTATUS* memory_status = (MEMORYSTATUS*)malloc(sizeof(MEMORYSTATUS)); 
  memory_status->dwLength = sizeof(MEMORYSTATUS);
  GlobalMemoryStatus(memory_status);
 
  MEMORYSTATUS var = *memory_status;
- 
- std::cout << var.dwTotalPhys; 
 
- return 0;
+ return var.dwTotalPhys;
 }
